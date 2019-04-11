31701b91b32a74f5c166fe105b2ae0817cb80749
Margus Veanes <margus@rackham2.uppmax.uu.se>
2019-04-11 18:06:26 +0200

    Genome Assembly and RNA Seq Correction Scripts Genome Assembly of PACBIO data via CANU RNA Seq Correction of Serum reads via TRIMMOMATIC

---
+++ scripts/Trimmomatic_Paper1_Untrimmed_RNASeq_Serum.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 06:30:00
+#SBATCH -J veanes1_rnaseq_trimmomatic
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Margus.Veanes.4784@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load trimmomatic
+
+trimmomatic PE \
+/proj/g2019003/nobackup/private/1_Zhang_2017/transcriptomics_data/RNA-Seq_Serum/untrimmed/ERR1797969_pass_1.fastq.gz \
+/proj/g2019003/nobackup/private/1_Zhang_2017/transcriptomics_data/RNA-Seq_Serum/untrimmed/ERR1797969_pass_2.fastq.gz \
+RNASeq1_BH_corrected_forward_paired.fq.gz RNASeq1_BH_corrected_forward_unpaired.fq.gz RNASeq1_BH_corrected_reverse_paired.fq.gz RNASeq1_BH_corrected_reverse_unpaired.fq.gz \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+trimmomatic PE \
+/proj/g2019003/nobackup/private/1_Zhang_2017/transcriptomics_data/RNA-Seq_Serum/untrimmed/ERR1797970_pass_1.fastq.gz \
+/proj/g2019003/nobackup/private/1_Zhang_2017/transcriptomics_data/RNA-Seq_Serum/untrimmed/ERR1797970_pass_2.fastq.gz \
+RNASeq2_BH_corrected_forward_paired.fq.gz RNASeq2_BH_corrected_forward_unpaired.fq.gz RNASeq2_BH_corrected_reverse_paired.fq.gz RNASeq2_BH_corrected_reverse_unpaired.fq.gz \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+trimmomatic PE \
+/proj/g2019003/nobackup/private/1_Zhang_2017/transcriptomics_data/RNA-Seq_Serum/untrimmed/ERR1797971_pass_1.fastq.gz \
+/proj/g2019003/nobackup/private/1_Zhang_2017/transcriptomics_data/RNA-Seq_Serum/untrimmed/ERR1797971_pass_2.fastq.gz \
+RNASeq3_BH_corrected_forward_paired.fq.gz RNASeq3_BH_corrected_forward_unpaired.fq.gz RNASeq3_BH_corrected_reverse_paired.fq.gz RNASeq3_BH_corrected_reverse_unpaired.fq.gz \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+
