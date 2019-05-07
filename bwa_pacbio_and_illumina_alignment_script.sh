515ca77b07a909b94380296634f3db751045baec
Margus Veanes <margus@rackham1.uppmax.uu.se>
2019-05-07 17:07:19 +0200

    Added Quast Results for Illumina Corrected Genome Assembly Included is the results folder of the Quast quality control analysis for the PacBio, Illumina corrected genome assembly

--- bwa_pacbio_and_illumina_alignment_script.sh
+++ bwa_pacbio_and_illumina_alignment_script.sh
@@ -10,12 +10,12 @@
 # Load modules
 module load bioinfo-tools
 module load bwa
-module load samtools
+module load samtools/1.9
 
 bwa index -p enterococci_pacbio_index /home/margus/paper1_analysis_files/canu_genome_assembly/Canu_Assembly_Output_Pacbio/Enterococci.contigs.fasta
 bwa mem -M enterococci_pacbio_index /proj/g2019003/nobackup/private/1_Zhang_2017/genomics_data/Illumina/E745-1.L500_SZAXPI015146-56_1_clean.fq.gz \
 /proj/g2019003/nobackup/private/1_Zhang_2017/genomics_data/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz > pacbio_illumina_aligned.sam \
-| sam sort -o pacbio_illumina_aligned.bam
+| samtools sort -o pacbio_illumina_aligned.bam
 
  
 
