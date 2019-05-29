5d83bb9c1503aa5d38020a44e0530abcf229e9ca
Margus Veanes <margus@rackham3.uppmax.uu.se>
2019-05-29 13:12:48 +0200

    Corrected Old synteny script Updated incorrect script

--- scripts/artemis_synteny_comp_script.sh
+++ scripts/artemis_synteny_comp_script.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 01:30:00
+#SBATCH -t 00:10:00
 #SBATCH -J veanes1_synteny_comp
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Margus.Veanes.4784@student.uu.se
@@ -10,5 +10,5 @@
 module load bioinfo-tools
 module load blast/2.7.1+
 
-makeblastdb -in /home/margus/reference.fasta -dbtype 'nucl' -out db/ref_db -parse_sequids
-blastn -db /home/margus/reference.fasta -query /home/margus/paper1_analysis_files/pilon_files/pilon_pacbio_illumina_combined_genome.fasta -outfmt 6 > efaecium_synteny.act
+makeblastdb -in /home/margus/Genome_Analysis_Paper1/synteny_sequence.fasta  -dbtype 'nucl' -out db/ref_db -parse_seqids
+blastn -db /domus/h1/margus/db/ref_db -query /home/margus/Genome_Analysis_Paper1/pilon_files/pilon_pacbio_illumina_combined_genome.fasta -outfmt 6 > combined_genome_vs_synteny_ref.act
