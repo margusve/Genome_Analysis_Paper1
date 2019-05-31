d46cec455a5dc9e0e46d72b6a1005cdcb00c7d00
Margus Veanes <margus@rackham2.uppmax.uu.se>
2019-05-31 23:29:13 +0200

    Changed filenames/foldernames Added clarity to names of folders, moved old *.ann files from rackham to github that were forgotten

---
+++ htseq_rna_counts/bh_count_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 03:00:00
+#SBATCH -J veanes_count_rna_bh
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Margus.Veanes.4784@student.uu.se
+
+##Counting BH Mapped RNA
+
+# Load modules
+
+module load bioinfo-tools
+module load samtools/1.9
+module load htseq/0.9.1
+
+samtools view /home/margus/paper1_analysis_files/rna_mapping/bh/bwa_output_rna_bh_map_1.bam | htseq-count --format bam --type CDS bwa_output_rna_bh_map_1.bam /home/margus/paper1_analysis_files/PROKKA_05072019_PacBio_Illumina_Corrected/annotated_combined_alignment.gtf > rna_bh_counts_1.txt
+
+
+module load bioinfo-tools
+module load samtools/1.9
+module load htseq/0.9.1
+
+
+samtools view /home/margus/paper1_analysis_files/rna_mapping/bh/bwa_output_rna_bh_map_2.bam | htseq-count --format bam --type CDS bwa_output_rna_bh_map_2.bam /home/margus/paper1_analysis_files/PROKKA_05072019_PacBio_Illumina_Corrected/annotated_combined_alignment.gtf > rna_bh_counts_2.txt
+
+
+module load bioinfo-tools
+module load samtools/1.9
+module load htseq/0.9.1
+
+
+samtools view /home/margus/paper1_analysis_files/rna_mapping/bh/bwa_output_rna_bh_map_3.bam | htseq-count --format bam --type CDS bwa_output_rna_bh_map_3.bam /home/margus/paper1_analysis_files/PROKKA_05072019_PacBio_Illumina_Corrected/annotated_combined_alignment.gtf > rna_bh_counts_3.txt
+
